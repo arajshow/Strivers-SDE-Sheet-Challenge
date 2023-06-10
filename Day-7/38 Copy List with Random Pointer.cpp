e415c49e662a4c869eb5bdd81556cdeeb89005a8
Anand Raj <20bec061@nith.ac.in>
2023-06-11 03:41:59 +0530

    Create 38 Copy List with Random Pointer.cpp

---
+++ Day-7/38 Copy List with Random Pointer.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+/*************************************************************
+
+    Following is the LinkedListNode class structure
+
+    template <typename T>   
+    class LinkedListNode
+    {
+        public:
+        T data;
+        LinkedListNode<T> *next;
+        LinkedListNode<T> *random;
+        LinkedListNode(T data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+
+*************************************************************/
+
+LinkedListNode<int> *cloneRandomList(LinkedListNode<int> *head)
+{
+    // Write your code here.
+    if(!head) return head;
+    LinkedListNode<int> *root = new LinkedListNode<int>(head->data);
+    root->next = head->next;
+    root->random = head->random;
+    return root;
+
+
+
+}
