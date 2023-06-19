6b3bd78677e990c3233e97f84dcee7685df58d64
Anand Raj <20bec061@nith.ac.in>
2023-06-19 19:05:34 +0530

    Create 93 Maximum In Sliding Windows Of Size K.cpp

---
+++ Day-15/93 Maximum In Sliding Windows Of Size K.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h> 
+vector<int> slidingWindowMaximum(vector<int> &nums, int &k)
+{
+    //    Write your code here.
+    deque<int> q;
+    vector<int> ans;
+
+    int n = nums.size();
+
+    for(int i=0; i<n; i++){
+        
+        if(!q.empty() && q.front()==i-k) q.pop_front();
+
+        while(!q.empty() && nums[q.back()]<nums[i]){
+            q.pop_back();
+        }
+
+        q.push_back(i);
+        
+        if(i>=k-1) ans.push_back(nums[q.front()]);
+    }
+
+    return ans;
+    
+}
